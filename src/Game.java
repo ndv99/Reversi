86ee3c82c95550bd3130adb5edd199ff5c8834f6
ndv99 <noodles524@gmail.com>
2018-11-21 21:59:37 +0000

    Finished algorithms to check for valid moves.

--- src/Game.java
+++ src/Game.java
@@ -37,6 +37,7 @@ class Game {
             if (validMoves.length > 0){
                 System.out.println("Player " + currentPlayer + ", it is your turn.");
                 System.out.println("Valid moves: " + String.join(" ,", validMoves));
+                gameScanner.nextLine();
             } else {
                 System.out.println("Player " + currentPlayer + ", you have no valid moves.");
             }
@@ -44,7 +45,7 @@ class Game {
         }
     }
 
-    void displayBoard(){
+    private void displayBoard(){
         System.out.println(" | A B C D E F G H");
         int currentRow = 1;
         for (String[] row : this.board){
@@ -93,119 +94,267 @@ class Game {
         return colIndex;
     }
 
+    private String indexToRow(int rowIndex){
+        return Integer.toString(rowIndex + 1);
+    }
+    
+    private String indexToCol(int colIndex){
+        String col = "A";
+        switch (colIndex){
+            case (1):
+                col = "B";
+                break;
+            case (2):
+                col = "C";
+                break;
+            case (3):
+                col = "D";
+                break;
+            case (4):
+                col = "E";
+                break;
+            case (5):
+                col = "F";
+                break;
+            case (6):
+                col = "G";
+                break;
+            case (7):
+                col = "H";
+                break;
+        }
+        return col;
+    }
+
     private String[] calculateValidMoves(){
         List<String> validMovesList = new ArrayList<>();
-        for (String[] row : this.board){
-            for (String cell: row){
-
+        for (int rowIndex = 0; rowIndex < 8; rowIndex++){
+            for (int colIndex = 0; colIndex < 8; colIndex++){
+                boolean validMove = checkHandler(rowIndex, colIndex);
+                if (validMove){
+                    String[] move = {indexToCol(colIndex), indexToRow(rowIndex)};
+                    validMovesList.add(String.join(" ", move));
+                }
             }
         }
-        String[] validMoves = validMovesList.toArray(new String[0]);
-        return validMoves;
+        return validMovesList.toArray(new String[0]);
     }
 
-    boolean checkN(String row, String col){
+    private boolean checkN(int rowIndex, int colIndex){
         boolean valid = false;
-        try{
-            boolean checked = false;
-            while (!checked){
 
+
+        boolean checked = false;
+        while (!checked){
+            try{
+                if (this.board[rowIndex - 1][colIndex].equals(opponentPiece)){
+                    valid = true;
+                    rowIndex --;
+                } else if (this.board[rowIndex - 1][colIndex].equals(playerPiece)){
+                    checked = true;
+                } else {
+                    valid = false;
+                    checked = true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                valid = false;
+                checked = true;
             }
-        } catch (ArrayIndexOutOfBoundsException e){
-
         }
         return valid;
     }
 
-    boolean checkNE(String row, String col){
+    private boolean checkNE(int rowIndex, int colIndex){
         boolean valid = false;
-        try{
-            boolean checked = false;
-            while (!checked){
 
+
+        boolean checked = false;
+        while (!checked){
+            try{
+                if (this.board[rowIndex - 1][colIndex + 1].equals(opponentPiece)){
+                    valid = true;
+                    rowIndex --;
+                    colIndex ++;
+                } else if (this.board[rowIndex - 1][colIndex + 1].equals(playerPiece)){
+                    checked = true;
+                } else {
+                    valid = false;
+                    checked = true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                valid = false;
+                checked = true;
             }
-        } catch (ArrayIndexOutOfBoundsException e){
-
         }
         return valid;
     }
 
-    boolean checkE(String row, String col){
+    private boolean checkE(int rowIndex, int colIndex){
         boolean valid = false;
-        try{
-            boolean checked = false;
-            while (!checked){
 
+
+        boolean checked = false;
+        while (!checked){
+            try{
+                if (this.board[rowIndex][colIndex + 1].equals(opponentPiece)){
+                    valid = true;
+                    colIndex ++;
+                } else if (this.board[rowIndex][colIndex + 1].equals(playerPiece)){
+                    checked = true;
+                } else {
+                    valid = false;
+                    checked = true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                valid = false;
+                checked = true;
             }
-        } catch (ArrayIndexOutOfBoundsException e){
-
         }
         return valid;
     }
 
-    boolean checkSE(String row, String col){
+    private boolean checkSE(int rowIndex, int colIndex){
         boolean valid = false;
-        try{
-            boolean checked = false;
-            while (!checked){
 
+
+        boolean checked = false;
+        while (!checked){
+            try{
+                if (this.board[rowIndex + 1][colIndex + 1].equals(opponentPiece)){
+                    valid = true;
+                    rowIndex ++;
+                    colIndex ++;
+                } else if (this.board[rowIndex + 1][colIndex + 1].equals(playerPiece)){
+                    checked = true;
+                } else {
+                    valid = false;
+                    checked = true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                valid = false;
+                checked = true;
             }
-        } catch (ArrayIndexOutOfBoundsException e){
-
         }
         return valid;
     }
 
-    boolean checkS(String row, String col){
+    private boolean checkS(int rowIndex, int colIndex){
         boolean valid = false;
-        try{
-            boolean checked = false;
-            while (!checked){
 
+
+        boolean checked = false;
+        while (!checked){
+            try{
+                if (this.board[rowIndex + 1][colIndex].equals(opponentPiece)){
+                    valid = true;
+                    rowIndex ++;
+                } else if (this.board[rowIndex + 1][colIndex].equals(playerPiece)){
+                    checked = true;
+                } else {
+                    valid = false;
+                    checked = true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                valid = false;
+                checked = true;
             }
-        } catch (ArrayIndexOutOfBoundsException e){
-
-        }
-        return valid;
-    }
-    boolean checkSW(String row, String col){
-        boolean valid = false;
-        try{
-            boolean checked = false;
-            while (!checked){
-
-            }
-        } catch (ArrayIndexOutOfBoundsException e){
-
-        }
-        return valid;
-    }
-
-    boolean checkW(String row, String col){
-        boolean valid = false;
-        try{
-            boolean checked = false;
-            while (!checked){
-
-            }
-        } catch (ArrayIndexOutOfBoundsException e){
-
         }
         return valid;
     }
 
-    boolean checkNW(String row, String col){
+    private boolean checkSW(int rowIndex, int colIndex){
         boolean valid = false;
-        try{
-            boolean checked = false;
-            while (!checked){
 
+
+        boolean checked = false;
+        while (!checked){
+            try{
+                if (this.board[rowIndex + 1][colIndex - 1].equals(opponentPiece)){
+                    valid = true;
+                    rowIndex ++;
+                    colIndex --;
+                } else if (this.board[rowIndex + 1][colIndex - 1].equals(playerPiece)){
+                    checked = true;
+                } else {
+                    valid = false;
+                    checked = true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                valid = false;
+                checked = true;
             }
-        } catch (ArrayIndexOutOfBoundsException e){
-
         }
         return valid;
     }
+
+    private boolean checkW(int rowIndex, int colIndex){
+        boolean valid = false;
+
+
+        boolean checked = false;
+        while (!checked){
+            try{
+                if (this.board[rowIndex][colIndex - 1].equals(opponentPiece)){
+                    valid = true;
+                    colIndex --;
+                } else if (this.board[rowIndex][colIndex - 1].equals(playerPiece)){
+                    checked = true;
+                } else {
+                    valid = false;
+                    checked = true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                valid = false;
+                checked = true;
+            }
+        }
+        return valid;
+    }
+
+    private boolean checkNW(int rowIndex, int colIndex){
+        boolean valid = false;
+
+
+        boolean checked = false;
+        while (!checked){
+            try{
+                if (this.board[rowIndex - 1][colIndex - 1].equals(opponentPiece)){
+                    valid = true;
+                    rowIndex --;
+                    colIndex --;
+                } else if (this.board[rowIndex - 1][colIndex - 1].equals(playerPiece)){
+                    checked = true;
+                } else {
+                    valid = false;
+                    checked = true;
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                valid = false;
+                checked = true;
+            }
+        }
+        return valid;
+    }
+
+    private boolean checkHandler(int rowIndex, int colIndex){
+        boolean valid = false;
+
+        boolean validN = checkN(rowIndex, colIndex);
+        boolean validNE = checkNE(rowIndex, colIndex);
+        boolean validE = checkE(rowIndex, colIndex);
+        boolean validSE = checkSE(rowIndex, colIndex);
+        boolean validS = checkS(rowIndex, colIndex);
+        boolean validSW = checkSW(rowIndex, colIndex);
+        boolean validW = checkW(rowIndex, colIndex);
+        boolean validNW = checkNW(rowIndex, colIndex);
+
+        if (validN || validNE || validE || validSE || validS || validSW || validW || validNW){
+            valid = true;
+        }
+        return valid;
+    }
+
+
 
     void calculateChanges(){}
 
